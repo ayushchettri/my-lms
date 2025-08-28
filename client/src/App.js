29d2cdd60ae0a782aaad4fcc05fb1fc74130d8ef
AyushChettri <r.buddha2661947@gmail.com>
2025-08-28 13:37:53 +0530

    landing-page-sist

--- client/src/App.js
+++ client/src/App.js
@@ -7,50 +7,62 @@ import rusa from './image/RUSA-ED.png'
 import su from './image/SU.png'
 import signIn from './image/login-avatar.png'
 
-function App() {
-
-  const [backendData, setBackendData] = useState([{}])
-  useEffect(()=>{
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+export function App () {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div id="container">
+      {/* Top Navigation */}
       <nav id="topNav">
-        <span id="topAddress"><p>Address: Chisopani, Jorethang, Sikkim-737121</p><p>Phone: +91 6294787509</p></span>
-        <span>
-          <button id="sign-in">Sign In</button>
-
-        </span>
+        <div id="topAddress">
+          <p>Address: Chisopani, Jorethang, Sikkim-737121</p>
+          <p>Phone: +91 6294787509</p>
+        </div>
+        <button id="sign-in">Sign In</button>
       </nav>
+
+      {/* Mid Navigation */}
       <div id="midNav">
-        <div id="sist-logo">
+        <div className="logo-group">
           <img src={sistLogo} alt="SIST-LOGO" />
         </div>
-        <div id="three-logo">
+        <div className="logo-group">
           <img src={aicte} alt="AICTE" />
           <img src={su} alt="SU" />
           <img src={rusa} alt="RUSA-ED" />
         </div>
       </div>
+
+      {/* Bottom Navigation */}
       <nav id="bottomNav">
-        <ul>
+        {/* Hamburger for Mobile */}
+        <div
+          className="hamburger"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Menu */}
+        <ul className={menuOpen ? "active" : ""}>
           <li><a href="#">HOME</a></li>
           <li><a href="#">ABOUT US</a></li>
           <li><a href="#">CONTACT US</a></li>
           <li><a href="#">DEVELOPER</a></li>
         </ul>
       </nav>
+
+      {/* Main Section */}
       <div id="main">
-        <button><img src={signIn} alt="SIGN IN" />SIGN IN</button>
+        <button>
+          <img src={signIn} alt="SIGN IN" />
+          SIGN IN
+        </button>
       </div>
     </div>
-  )
+  );
+
 }
 
 export default App
