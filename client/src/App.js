4a2a847f305ed46d01f86241c50ba8aaf10d8918
AyushChettri <r.buddha2661947@gmail.com>
2025-08-29 14:22:05 +0530

    new-landing page(standard white theme)

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,66 @@
 import React, {useEffect, useState} from 'react'
 import './App.css'
 import './index.css'
-import sistLogo from './image/sist-logo.png'
-import aicte from './image/AICTE.png'
-import rusa from './image/RUSA-ED.png'
-import su from './image/SU.png'
-import signIn from './image/login-avatar.png'
+import logo from './image/sist-logo.png'
+import bgImage from './image/sist-banner.jpg'
 
 export function App () {
   const [menuOpen, setMenuOpen] = useState(false);
-  return (
-    <div id="container">
-      {/* Top Navigation */}
-      <nav id="topNav">
-        <div id="topAddress">
-          <p>Address: Chisopani, Jorethang, Sikkim-737121</p>
-          <p>Phone: +91 6294787509</p>
-        </div>
-        <button id="sign-in">Sign In</button>
-      </nav>
-
-      {/* Mid Navigation */}
-      <div id="midNav">
-        <div className="logo-group">
-          <img src={sistLogo} alt="SIST-LOGO" />
-        </div>
-        <div className="logo-group">
-          <img src={aicte} alt="AICTE" />
-          <img src={su} alt="SU" />
-          <img src={rusa} alt="RUSA-ED" />
-        </div>
-      </div>
-
-      {/* Bottom Navigation */}
-      <nav id="bottomNav">
-        {/* Hamburger for Mobile */}
-        <div
-          className="hamburger"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
+    return (
+    <div className="container">
+      {/* Navbar */}
+      <header className="navbar">
+        {/* Logo */}
+        <div className="logo-section">
+          <img src={logo} alt="Logo" className="logo" />
         </div>
 
         {/* Menu */}
-        <ul className={menuOpen ? "active" : ""}>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">ABOUT US</a></li>
-          <li><a href="#">CONTACT US</a></li>
-          <li><a href="#">DEVELOPER</a></li>
-        </ul>
-      </nav>
+        <nav className="menu">
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+          <a href="#contact">Contact Us</a>
+          <a href="#developers">Developers</a>
+          <button className="login-btn">Login</button>
+        </nav>
+      </header>
 
-      {/* Main Section */}
-      <div id="main">
-        <button>
-          <img src={signIn} alt="SIGN IN" />
-          SIGN IN
-        </button>
-      </div>
+      {/* Title */}
+      <main className="main-section">
+        <h1 className="title">
+          SIST <span className="highlight">LMS</span>
+        </h1>
+
+        {/* Background Image Section */}
+        <div className="image-container">
+          <img src={bgImage} alt="Campus" className="main-image" />
+          <button className="overlay-login-btn">Login</button>
+        </div>
+
+        {/* Footer Section */}
+        <footer className="footer">
+          <div className="footer1">
+            <p className="college-name">Sikkim Institute of Science and Technology</p>
+            <p className="address">Chisopani, Jorethang, Sikkim- 737121</p>
+            <div className="social-icons">
+              <a href="https://www.facebook.com/profile.php?id=100057201619385"><i className="fab fa-facebook"></i></a>
+              <a href="https://www.instagram.com/sist__college2018"><i className="fab fa-instagram"></i></a>
+            </div>
+          </div>
+          
+
+          {/* Topics */}
+          <div className="footer2">
+            <p>Topic</p>
+            <a href="https://sist.edu.in">About Us</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Developers</a>
+          </div>
+        </footer>
+      </main>
     </div>
   );
+
 
 }
 
